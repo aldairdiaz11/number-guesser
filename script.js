750bbe2362ac1e47699ab4f9867794f709a9d5c1
aldairdiaz <irving.s21diaz@gmail.com>
2022-12-22 15:27:11 -0600

    Final commit

--- script.js
+++ script.js
@@ -3,4 +3,23 @@ let computerScore = 0;
 let currentRoundNumber = 1;
 
 // Write your code below:
+const generateTarget = () => Math.floor(Math.random() * 10);
 
+const compareGuesses = (human, computer, secretTarget) => {
+    const humanDifference = Math.abs(human - secretTarget);
+    const computerDifference = Math.abs(computer - secretTarget)
+    if (humanDifference === computerDifference)
+        return true;
+    else return humanDifference < computerDifference;
+}
+
+const updateScore = (winner) => {
+    if (winner === 'human')
+        humanScore += 1;
+    else
+        computerScore += 1;
+}
+
+const advanceRound = () => {
+    currentRoundNumber += 1;
+}
